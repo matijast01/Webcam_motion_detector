4a0621288ba0dd44803bd832675f155880f4365c
Matijast <matijastipcevic01@yahoo.com>
2022-12-05 15:23:49 +0000

    Detect object and react when object has left the frame

---
+++ main.py
@@ -0,0 +1,66 @@
+import cv2
+import time
+from emailing import send_email
+
+video = cv2.VideoCapture(0)
+time.sleep(1)
+
+first_frame = None
+status_list = []
+
+while True:
+    status = 0
+    check, frame = video.read()
+
+    # Grayscale and Gaussian Blur to reduce data size while computing data
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+
+    # Save first frame data
+    if first_frame is None:
+        first_frame = gray_frame_gau
+
+    # Get the delta frame
+    delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
+
+    # Get a frame based on a threshold
+    thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
+
+    # dilate does something idk
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
+
+    cv2.imshow("My Video", dil_frame)
+
+    # Find contours of the objects shown by dil_frame
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # check all contours
+    for contour in contours:
+
+        # if Contour is small consider it a false object
+        if cv2.contourArea(contour) < 5000:
+            continue
+        # get a bounding rectangle for the contour and draw it on color frame
+        x, y, w, h = cv2.boundingRect(contour)
+        rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+        # check if object has entered frame
+        if rectangle.any():
+            status = 1
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email()
+
+    print(status_list)
+
+    cv2.imshow("Video", frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord("q"):
+        break
+
+video.release()
